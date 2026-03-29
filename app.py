ff1da0b2c8defde7b30a825f49c28d6026c9c447
Shubhamkhatri3 <shubhamkhatri563@gmail.com>
2026-03-29 18:01:30 +0530

    Fixed port for Render deployment

--- app.py
+++ app.py
@@ -85,6 +85,9 @@ async def home(request: Request):
         {"request": request}
     )
 
+import os
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
